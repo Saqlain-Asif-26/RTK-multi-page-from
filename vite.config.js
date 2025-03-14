a4889b08a1d6159b063ae7df1696b0b0d6c3eafa
saqlain-asif-26 <saqlainasif26@gmail.com>
2025-03-14 21:18:12 +0500

    Fixed vite config and added esbuild settings

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/RTK-multi-page-from/',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
 })
