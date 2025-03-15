43b08a6bd02a30fdf6570f2477f11d30ce9fa254
Saqlain-Asif-26 <rsaqlainasif26@gmail.com>
2025-03-15 03:05:37 +0000

    Deploying to gh-pages from @ Saqlain-Asif-26/RTK-multi-page-from@661f1e6e028687c4381cc3111a63594374609535 🚀

--- src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import formSlice from "./Slices/FormSlice";
-
-export const store = configureStore({
-  reducer: {
-    nameSlice: formSlice,
-  },
-})
