1d87f48b5f29ae014c8040a39011d59b1168e311
saqlain-asif-26 <saqlainasif26@gmail.com>
2025-03-15 08:11:57 +0500

    Update Step4 styling

--- src/Components/Step4.jsx
+++ src/Components/Step4.jsx
@@ -14,9 +14,9 @@ function Step4() {
   return (
     <div>
         <div className='my-4'>
-            <ul className='text-start border-2 border-green-500 pb-4 rounded-lg'>
+            <ul className='text-start border-2 border-green-500 rounded-lg'>
                 <h1 className='text-2xl text-center text-green-700 font-bold border-b border-green-500'>Form Submitted</h1>
-                <div className='px-4'>
+                <div className='px-4 py-2'>
                     <p className='text-md font-semibold inline'>Name: </p>
                     <li className='text-lg font-bold inline'>{formData.name}</li><br />
                     <p className='text-md font-semibold inline'>Email: </p>
